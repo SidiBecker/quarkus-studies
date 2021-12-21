03871d7f56b82a93aaa5329ef2cbdc6cd86df055
Sidi Becker <sidibecker@hotmail.com>
2021-12-21 10:40:09 -0300

    first commit

---
+++ src/test/java/com/sidneibecker/NativeHelloIT.java
@@ -0,0 +1,9 @@
+package com.sidneibecker;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloIT extends HelloTest {
+
+    // Execute the same tests but in native mode.
+}
